f38e603521fd819e764016479ac00c71677c3c52
Samuel Pagenkopf <ssaammp@gmail.com>
2018-12-02 12:51:18 -0600

    smarter comments

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// TODO: cursor navigation
+// TODO: compile and run code
+// TODO: clean up innercode for better editing (i.e. ;;;; comments)
+
 let code = document.getElementById('code');
 let screen = document.getElementById('screen').getContext('2d');
 let innercode = code.innerHTML;
@@ -7,14 +11,24 @@ const format = (lines) => lines.split('\n')
       .reduce((acc, v) => acc + v);
 
 const format_line = (line) => {
-    const [, code, comment] = line.match(/([^;]*);?(.*)/);
+    const [, code, comment] = line.match(/([^;]*)[;\s]*(.*)/);
+    // special case for single-line comment
+    if (code === '' && comment !== '') {
+        return `<div class=c_comment> ;;; ${comment}</div>\n`;
+    }
     const [, label, rest] = code.match(/(\w+:|)\s*(.*)/);
+
+    const label_code = `<div class="c_label">${label}</div>`;
+
+    // special case for label w/o code
     const code_code = label !== '' && rest === '' ?
           '<div class="c_separator"> </div>' :
           `<div class="c_opcode">${rest}</div>`;
-    return `<div class="c_label">${label}</div>\
-${code_code}\
-<div class="c_comment">${comment}</div>\n`;
+
+    const comment_code = comment === '' ? '' :
+          `<div class="c_comment">; ${comment}</div>`;
+
+    return label_code + code_code + comment_code + '\n';
 };
 
 const tick = () => {
@@ -35,7 +49,8 @@ refresh();
 const modkeys = {
     Shift: 'up',
 };
-const inputkeys = /^[a-zA-Z;: ]$/;
+// Any single-char key is probably okay...right?
+const inputkeys = /^.$/u;
 
 document.onkeydown = (event) => {
     let key = event.key;
